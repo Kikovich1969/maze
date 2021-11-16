c1c222194e7ac2c02814fe4f06d3016148fd5e19
Christoph Meissl <meissl.christoph@gmail.com>
2021-11-16 23:30:57 +0100

    Deleted class Cell; Cell is now part of class Maze

--- maze.js
+++ maze.js
@@ -5,80 +5,153 @@ let lineWidth = 8; // must be even, otherwise antialiasing will show!
 let game = {
   width: 600,
   height: 400,
-  backgroundColor: "black",
+  backgroundColor: "white",
 };
 
 class Maze {
   constructor(cols, rows) {
     this.cols = cols;
     this.rows = rows;
+    this.cell = {};
     this.cells = [];
+    this.activeCell = undefined;
+    this.nextCell = undefined;
+    this.createGrid();
+    this.setCellNeighbours();
+    this.setCellVertices();
+    this.setActiveCell(this.getRandomCell());
+    this.setNextCell(this.activeCell);
+    this.drawMaze();
   }
-  getRandomCell = () => {
-    let randomIndex = generateRandomIntegerInRange(0, this.cells.length - 1);
-    return this.cells[randomIndex];
+
+  createGrid = () => {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        let cell = {
+          x: j,
+          y: i,
+          width: Math.floor(game.width / this.cols),
+          height: Math.floor(game.height / this.rows),
+          visited: false,
+          neighbours: [],
+          vertices: {
+            tl: { x: undefined, y: undefined } /* Top Left */,
+            tr: { x: undefined, y: undefined } /* Top Right */,
+            bl: { x: undefined, y: undefined } /* Bottom Left */,
+            br: { x: undefined, y: undefined } /* Bottom Right */,
+          },
+          walls: { top: true, right: true, bottom: true, left: true },
+        };
+        this.cells.push(cell);
+      }
+    }
   };
-  setNextCell = (activeCell) => {
-    console.log(activeCell.neighbours);
-  };
-  drawMaze = () => {
-    ctx.strokeStyle = "#ffffff";
-    ctx.lineWidth = lineWidth;
-    this.cells.forEach((element) => {
-      /* console.log(element.vertices); */
-      ctx.moveTo(element.vertices.tl.x, element.vertices.tl.y);
-      ctx.lineTo(element.vertices.tr.x, element.vertices.tr.y);
-      ctx.lineTo(element.vertices.br.x, element.vertices.br.y);
-      ctx.lineTo(element.vertices.bl.x, element.vertices.bl.y);
-      ctx.lineTo(element.vertices.tl.x, element.vertices.tl.y);
-      ctx.stroke();
+
+  setCellNeighbours = () => {
+    this.cells.forEach((cell, index) => {
+      if (!(cell.y === 0)) {
+        cell.neighbours.push({ index: index - this.cols, direction: "top" });
+      }
+      if (!(cell.x === this.cols - 1)) {
+        cell.neighbours.push({ index: index + 1, direction: "right" });
+      }
+      if (!(cell.y === this.rows - 1)) {
+        cell.neighbours.push({
+          index: index + this.cols,
+          direction: "bottom",
+        });
+      }
+      if (!(cell.x === 0)) {
+        cell.neighbours.push({
+          index: index - 1,
+          direction: "left",
+        });
+      }
     });
   };
-}
 
-class Cell extends Maze {
-  constructor(cols, rows, x, y) {
-    super(cols, rows);
-    this.width = Math.floor(game.width / this.cols);
-    this.height = Math.floor(game.height / this.rows);
-    this.x = x;
-    this.y = y;
-    this.visited = false;
-    this.neighbours = [];
-    this.vertices = {
-      tl: { x: undefined, y: undefined } /* Top Left */,
-      tr: { x: undefined, y: undefined } /* Top Right */,
-      bl: { x: undefined, y: undefined } /* Bottom Left */,
-      br: { x: undefined, y: undefined } /* Bottom Right */,
-    };
-    this.walls = { top: true, right: true, bottom: true, left: true };
-  }
-  setNeighbours = (index) => {
-    if (!(this.y === 0)) {
-      this.neighbours.push({ index: index - this.cols, direction: "top" });
-    }
-    if (!(this.x === this.cols - 1)) {
-      this.neighbours.push({ index: index + 1, direction: "right" });
-    }
-    if (!(this.y === this.rows - 1)) {
-      this.neighbours.push({ index: index + this.cols, direction: "bottom" });
-    }
-    if (!(this.x === 0)) {
-      this.neighbours.push({ index: index - 1, direction: "left" });
-    }
+  setCellVertices = () => {
+    this.cells.forEach((cell) => {
+      cell.vertices.tl.x = cell.x * cell.width;
+      cell.vertices.tl.y = cell.y * cell.height;
+      cell.vertices.tr.x = cell.vertices.tl.x + cell.width;
+      cell.vertices.tr.y = cell.vertices.tl.y;
+      cell.vertices.bl.x = cell.vertices.tl.x;
+      cell.vertices.bl.y = cell.y * cell.height + cell.height;
+      cell.vertices.br.x = cell.vertices.tl.x + cell.width;
+      cell.vertices.br.y = cell.vertices.tr.y + cell.height;
+    });
   };
-  setVertices = () => {
-    this.vertices.tl.x = this.x * this.width;
-    this.vertices.tl.y = this.y * this.height;
-    this.vertices.tr.x = this.vertices.tl.x + this.width;
-    this.vertices.tr.y = this.vertices.tl.y;
-    this.vertices.bl.x = this.vertices.tl.x;
-    this.vertices.bl.y = this.y * this.height + this.height;
-    this.vertices.br.x = this.vertices.tl.x + this.width;
-    this.vertices.br.y = this.vertices.tr.y + this.height;
+
+  getRandomCell = () => {
+    let randomIndex = generateRandomIntegerInRange(0, this.cells.length - 1);
+    return randomIndex;
   };
-  show = () => {
-    console.log("Show Cell");
+
+  setActiveCell = (index) => {
+    this.activeCell = this.cells[index];
+    this.activeCell.visited = true;
+  };
+
+  setNextCell = (activeCell) => {
+    console.log(activeCell.neighbours);
+    this.nextCell = undefined;
+    for (let i = 0; i < activeCell.neighbours.length; i++) {
+      if (this.cells[activeCell.neighbours[i].index].visited === false) {
+        let direction = activeCell.neighbours[i].direction;
+        this.nextCell = this.cells[activeCell.neighbours[i].index];
+        this.nextCell.visited = true;
+        switch (direction) {
+          case "top":
+            this.activeCell.walls.top = false;
+            this.nextCell.walls.bottom = false;
+            break;
+          case "right":
+            this.activeCell.walls.right = false;
+            this.nextCell.walls.left = false;
+            break;
+          case "bottom":
+            this.activeCell.walls.bottom = false;
+            this.nextCell.walls.top = false;
+            break;
+          case "left":
+            this.activeCell.walls.left = false;
+            this.nextCell.walls.right = false;
+            break;
+        }
+        break;
+      }
+    }
+    if (this.nextCell === undefined) {
+      console.log("Backtracking start");
+    }
+    console.log(this.nextCell);
+    console.log(this.activeCell);
+  };
+
+  drawMaze = () => {
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = lineWidth;
+    this.cells.forEach((cell) => {
+      /* console.log(element.vertices); */
+      if (cell.walls.top) {
+        ctx.moveTo(cell.vertices.tl.x, cell.vertices.tl.y);
+        ctx.lineTo(cell.vertices.tr.x, cell.vertices.tr.y);
+      }
+      if (cell.walls.right) {
+        ctx.moveTo(cell.vertices.tr.x, cell.vertices.tr.y);
+        ctx.lineTo(cell.vertices.br.x, cell.vertices.br.y);
+      }
+      if (cell.walls.bottom) {
+        ctx.moveTo(cell.vertices.br.x, cell.vertices.br.y);
+        ctx.lineTo(cell.vertices.bl.x, cell.vertices.bl.y);
+      }
+      if (cell.walls.left) {
+        ctx.moveTo(cell.vertices.bl.x, cell.vertices.bl.y);
+        ctx.lineTo(cell.vertices.tl.x, cell.vertices.tl.y);
+      }
+      ctx.stroke();
+    });
   };
 }
 
@@ -87,101 +160,8 @@ function init() {
   canvas.style.height = game.height;
   canvas.style.backgroundColor = game.backgroundColor;
 
-  let maze = new Maze(6, 4);
-  for (let i = 0; i < maze.rows; i++) {
-    for (let j = 0; j < maze.cols; j++) {
-      let cell = new Cell(maze.cols, maze.rows, j, i);
-      let index = maze.cells.push(cell) - 1;
-      cell.setNeighbours(index);
-      cell.setVertices();
-      //cell.show();
-    }
-  }
-  maze.drawMaze(canvas);
-  /* let randomIndex = generateRandomIntegerInRange(0, maze.cells.length - 1);
-  let activeCell = maze.cells[randomIndex]; */
-  let activeCell = maze.getRandomCell();
-  activeCell.visited = true;
-
-  /* Mark active cell */
-  /* ctx.fillStyle = "green";
-  ctx.fillRect(
-    activeCell.vertices.tl.x,
-    activeCell.vertices.tl.y,
-    activeCell.width,
-    activeCell.height
-  ); */
-
-  /* Choose neighbour which was not visitied yet */
-  for (let i = 0; i < activeCell.neighbours.length; i++) {
-    let newCellIndex = activeCell.neighbours[i].index;
-    let direction = activeCell.neighbours[i].direction;
-    console.log(newCellIndex);
-    if (!maze.cells[newCellIndex].visited) {
-      newActiveCell = maze.cells[newCellIndex];
-      newActiveCell.visited = true;
-      /* Delete Lines */
-      if (direction === "top") {
-        ctx.beginPath();
-        ctx.moveTo(activeCell.vertices.tl.x, activeCell.vertices.tl.y);
-        ctx.lineTo(activeCell.vertices.tr.x, activeCell.vertices.tr.y);
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = lineWidth;
-        ctx.stroke();
-        ctx.moveTo(newActiveCell.vertices.bl.x, newActiveCell.vertices.bl.y);
-        ctx.lineTo(newActiveCell.vertices.br.x, newActiveCell.vertices.br.y);
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = lineWidth;
-        ctx.stroke();
-      } else if (direction === "right") {
-        ctx.beginPath();
-        ctx.moveTo(activeCell.vertices.tr.x, activeCell.vertices.tr.y);
-        ctx.lineTo(activeCell.vertices.br.x, activeCell.vertices.br.y);
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = lineWidth;
-        ctx.stroke();
-        ctx.moveTo(newActiveCell.vertices.tl.x, newActiveCell.vertices.tl.y);
-        ctx.lineTo(newActiveCell.vertices.bl.x, newActiveCell.vertices.bl.y);
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = lineWidth;
-        ctx.stroke();
-      } else if (direction === "bottom") {
-        ctx.beginPath();
-        ctx.moveTo(activeCell.vertices.bl.x, activeCell.vertices.bl.y);
-        ctx.lineTo(activeCell.vertices.br.x, activeCell.vertices.br.y);
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = lineWidth;
-        ctx.stroke();
-        ctx.moveTo(newActiveCell.vertices.tl.x, newActiveCell.vertices.tl.y);
-        ctx.lineTo(newActiveCell.vertices.tr.x, newActiveCell.vertices.tr.y);
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = lineWidth;
-        ctx.stroke();
-      } else {
-        /* direction left */
-        ctx.beginPath();
-        ctx.moveTo(activeCell.vertices.bl.x, activeCell.vertices.bl.y);
-        ctx.lineTo(activeCell.vertices.tl.x, activeCell.vertices.tl.y);
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = lineWidth + 2;
-        ctx.stroke();
-        ctx.moveTo(newActiveCell.vertices.tr.x, newActiveCell.vertices.tr.y);
-        ctx.lineTo(newActiveCell.vertices.br.x, newActiveCell.vertices.br.y);
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = lineWidth + 2;
-        ctx.stroke();
-      }
-
-      console.log("New active cell:");
-      console.log(newActiveCell);
-      activeCell = newActiveCell;
-      break;
-    } else {
-      /* No cell  */
-      console.log("Backtracking Start");
-    }
-  }
-  //console.log(maze.cells);
+  let maze = new Maze(5, 4);
+  console.log(maze.cells);
 }
 
 window.onload = () => {
